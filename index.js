4e5d4941769289c2b637dae17cb92ff3b811c4c3
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-06-06 11:35:13 +0200

    Updating comments

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ MongoClient.connect(
 	DB_URL,
 	function(err, client) {
 		if (err) {
-			console.error(error);
+			console.error(err);
 		} else {
 			console.log("Connected successfully to server");
 
@@ -23,33 +23,53 @@ MongoClient.connect(
 	}
 );
 
-async function comment(repo, issue, files) {
-	const body = `\
-# screenshot-tester report
-
-${files
-		.map(v => `${DOMAIN}/${repo}/${issue}/${v}.png`)
-		.map(v => `![](${v})`)
-		.join("\n")}`;
-
-	return await request({
-		method: "POST",
+const github = (method, url, body = undefined) =>
+	request({
+		method: method,
 		auth: {
 			user: "mischnic",
 			pass: TOKEN
 		},
 		headers: {
-			"User-Agent": "mischnic - screenshot-tester-server"
+			"User-Agent": "mischnic - screenshot-tester-server",
+			Accept: "application/json"
 		},
-		uri: `https://api.github.com/repos/${repo}/issues/${issue}/comments`,
+		uri: url,
 		json: {
-			body
+			body: body
 		}
 	});
+
+function generateBody(id, files) {
+	files = files.map(
+		v => (v.indexOf(DOMAIN) == -1 ? `${DOMAIN}/${id}/${v}.png` : v)
+	);
+
+	return `\
+<!--
+${JSON.stringify(files)}
+-->
+# screenshot-tester report
+
+${files.map(v => `![](${v})`).join("\n")}
+
+*This comment was created automatically by screenshot-tester-server.*`;
+}
+
+async function updateComment(id, url, files) {
+	return github("PATCH", url, generateBody(id, files));
+}
+
+function comment(repo, issue, files) {
+	return github(
+		"POST",
+		`https://api.github.com/repos/${repo}/issues/${issue}/comments`,
+		generateBody(`${repo}/${issue}`, files)
+	);
 }
 
 const regexPOST = /\/([\w-]+\/[\w-]+)\/([0-9]+)/;
-const regexGET = /\/([\w-]+\/[\w-]+)\/([0-9]+)\/([\w-.]+)\.png/;
+const regexGET = /\/([\w-]+\/[\w-]+\/[0-9]+)\/([\w-.]+)\.png/;
 
 module.exports = upload(async (req, res) => {
 	if (collection) {
@@ -59,30 +79,61 @@ module.exports = upload(async (req, res) => {
 				const [_, repo, issue] = match;
 				const id = `${repo}/${issue}`;
 
-				const doc = { id };
+				let doc = { files: {}, id };
 				for (let file of Object.keys(req.files)) {
 					await move(req.files[file], "/tmp/sts_temp");
 
 					const fileData = await fs.readFile("/tmp/sts_temp");
-					doc[file] = Binary(fileData);
+					doc.files[file] = Binary(fileData);
 				}
-				await collection.findOneAndReplace({ id }, doc, {
-					upsert: true
-				});
-				await comment(repo, issue, Object.keys(req.files));
+
+				const oldDoc = await collection.findOne({ id });
+				if (oldDoc && oldDoc.comment_url) {
+					doc = {
+						...oldDoc,
+						...doc,
+						files: { ...oldDoc.files, ...doc.files }
+					};
+					const files = [
+						...Object.keys(oldDoc.files),
+						...Object.keys(req.files)
+					];
+
+					await updateComment(id, oldDoc.comment_url, files);
+					await collection.findOneAndReplace({ id }, doc, {
+						upsert: true
+					});
+				} else {
+					const { url: comment_url } = await comment(
+						repo,
+						issue,
+						Object.keys(req.files)
+					);
+					doc.comment_url = comment_url;
+
+					await collection.findOneAndReplace({ id }, doc, {
+						upsert: true
+					});
+				}
+
 				return send(res, 200);
 			}
 		} else if (req.method == "GET") {
 			const match = req.url.match(regexGET);
 			if (match) {
-				const [_, repo, issue, file] = match;
+				const [_, id, file] = match;
 
 				const doc = await collection.findOne({
-					id: `${repo}/${issue}`
+					id
 				});
-				if (doc[file] && doc[file].buffer) {
+				if (!doc) {
+					return send(res, 404);
+				}
+				if (doc && doc.files[file] && doc.files[file].buffer) {
 					res.setHeader("Content-Type", "image/png");
-					return send(res, 200, doc[file].buffer);
+					return send(res, 200, doc.files[file].buffer);
+				} else {
+					return send(res, 500);
 				}
 			}
 		}
